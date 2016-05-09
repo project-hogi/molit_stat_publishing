1e3bfeed8804fe586898f1677fe5c7447e6b9ea5
teatime <3yong2ya@naver.com>
2016-05-09 13:46:14 +0900

    !!!

--- www/resources/front/js/script.js
+++ www/resources/front/js/script.js
@@ -1,12 +1,12 @@
 var lib = [
-  	"../resources/front-vendor/jquery-ui.min.js",
-   	"../resources/front-vendor/bootstrap.min.js",
-	"../resources/front/js/wrap/module-modal-box.js",
-	"../resources/front/js/wrap/module-modal-box.setting.js",
-	"../resources/front/js/wrap/module-scroll-lock.js",
-	"../resources/front/js/wrap/module-smoothscroll.js",
-	"../resources/front/js/wrap/common.js",
-	"../resources/front/js/wrap/app.js"
+  	"/resources/front-vendor/jquery-ui.min.js",
+   	"/resources/front-vendor/bootstrap.min.js",
+	"/resources/front/js/wrap/module-modal-box.js",
+	"/resources/front/js/wrap/module-modal-box.setting.js",
+	"/resources/front/js/wrap/module-scroll-lock.js",
+	"/resources/front/js/wrap/module-smoothscroll.js",
+	"/resources/front/js/wrap/common.js",
+	"/resources/front/js/wrap/app.js"
 ];
 
 for(var idx in lib)
